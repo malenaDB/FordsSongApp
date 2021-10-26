56fbfb9303e5bbefccc494ee23cd77ad2c48c555
malenaDB <malena.duquebaird@gmail.com>
2021-10-26 12:13:34 -0400

    Initial Commit

---
+++ FordsSongApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FordsSongApp
+//
+//  Created by Malena on 10/26/21.
+//  Copyright © 2021 MDB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
